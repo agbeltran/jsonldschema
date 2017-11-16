986b85c0d3f6f00082a62cf3a331012f64ea5f77
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-11-16 09:47:50 +0000

    json schema validator + instance validator

---
+++ util/jsonschema_validator.py
@@ -0,0 +1,43 @@
+import os
+from os.path import  join
+import json
+from jsonschema.validators import (
+     Draft4Validator, RefResolver
+)
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def validate_schema(path, schema_file_name):
+    try:
+        logger.info("Validating schema %s", schema_file_name)
+        with open(join(path, schema_file_name)) as schema_file:
+            schema = json.load(schema_file)
+            Draft4Validator.check_schema(schema)
+            return True
+    except Exception as e:
+        logger.error(e)
+        logger.info("done.")
+        return False
+
+def validate_instance(schemapath, schemafile, instancepath, instancefile, error_printing, store):
+        instancefile_fullpath = os.path.join(instancepath, instancefile)
+        instance = json.load(open(instancefile_fullpath))
+        schemafile_fullpath = os.path.join(schemapath, schemafile)
+        schema = json.load(open(schemafile_fullpath))
+        resolver = RefResolver('file://' + schemapath + '/' + schemafile, schema, store)
+        validator = Draft4Validator(schema, resolver=resolver)
+        logger.info("Validating instance %s against schema %s", instancefile_fullpath, schemafile_fullpath)
+        if (error_printing == 0):
+            errors = sorted(validator.iter_errors(instance), key=lambda e: e.path)
+            for error in errors:
+                print(error.message)
+        elif (error_printing == 1):
+            errors = sorted(validator.iter_errors(instance), key=lambda e: e.path)
+
+        for error in errors:
+            for suberror in sorted(error.context, key=lambda e: e.schema_path):
+                print(list(suberror.schema_path), suberror.message, sep=", ")
+        else:
+            validator.validate(instance, schema)
