9b81870e0490362a59f100a1d86bf89c714a191d
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2018-01-09 17:36:29 +0000

    Refactored tests.

--- tests/test_jsonschema2cedar.py
+++ tests/test_jsonschema2cedar.py
@@ -23,45 +23,26 @@ class TestJSONschema2cedar(unittest.TestCase):
         self.assertTrue(response["errors"] == [])
 
 
-    def convert(self, schema_filename, cedar_schema_filename):
+    def convert(self, schema_filename, cedar_schema_filename, conversion_output_filename):
         full_schema_filename = os.path.join(self._data_dir, schema_filename)
         output_schema = cedar.jsonschema2cedar.convert_template_element(full_schema_filename)
         output_schema_json = json.loads(output_schema)
         cedar_schema_file = open(os.path.join(self._data_dir, cedar_schema_filename), "rb")
         cedar_schema_json = json.load(cedar_schema_file)
-        comparison = equal_dicts(output_schema_json, vendor_cedar_schema_json, cedar.jsonschema2cedar.IGNORE_KEYS)
+        comparison = equal_dicts(output_schema_json, cedar_schema_json, cedar.jsonschema2cedar.IGNORE_KEYS)
         print(comparison)
 
-    def test_convert_vendor(self):
-        vendor_schema = os.path.join(self._data_dir, "vendor_schema.json")
-        output_schema = cedar.jsonschema2cedar.convert_template_element(vendor_schema)
-        output_schema_json = json.loads(output_schema)
-        ### compare json schema output with json schema produced by cedar tool (minus UI specific values)
-        vendor_cedar_schema_file = open(os.path.join(self._data_dir, 'vendor_cedar_schema.json'), "rb")
-        vendor_cedar_schema_json = json.load(vendor_cedar_schema_file)
-        comparison = equal_dicts(output_schema_json, vendor_cedar_schema_json, cedar.jsonschema2cedar.IGNORE_KEYS)
-        print(comparison)
-
-        self.validate_converted_file(vendor_cedar_schema_file, "production", "agb_production.apiKey")
-
-        vendor_cedar_schema_file.close()
-
-        outfile  = open(os.path.join(self._data_dir, 'vendor_cedar_schema_out.json'), "w")
+        ### save converted file
+        outfile = open(os.path.join(self._data_dir, conversion_output_filename), "w")
         json_pretty_dump(output_schema_json, outfile)
+
+        ### validate converted file
+        self.validate_converted_file(output_schema, "production", "agb_production.apiKey")
+        cedar_schema_file.close()
         outfile.close()
 
+    def test_convert_vendor(self):
+        self.convert("vendor_schema.json", 'vendor_cedar_schema.json', 'vendor_cedar_schema_out.json')
 
     def test_convert_sample(self):
-        sample_schema = os.path.join(self._data_dir, "sample_schema.json")
-        sample_cedar_schema = cedar.jsonschema2cedar.convert_template_element(sample_schema)
-        output_schema_json = json.loads(sample_cedar_schema)
-
-        sample_cedar_schema_file = open(os.path.join(self._data_dir, 'sample_cedar_schema.json'), "rb")
-        sample_cedar_schema_json = json.load(sample_cedar_schema_file)
-        comparison = equal_dicts(output_schema_json, sample_cedar_schema_json, cedar.jsonschema2cedar.IGNORE_KEYS)
-        print(comparison)
-        sample_cedar_schema_file.close()
-
-        #print(sample_cedar_schema)
-        self.validate_converted_file(sample_cedar_schema, "production", "agb_production.apiKey")
-
+        self.convert("sample_schema.json", 'sample_cedar_schema.json', 'sample_cedar_schema_out.json')
