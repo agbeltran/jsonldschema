5e767ec6b8751c1e2a8651fe80ea745fdb161f0b
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2018-01-10 11:57:07 +0000

    Tests to validate json-schemas and fixes.

--- tests/test_jsonschema_validator.py
+++ tests/test_jsonschema_validator.py
@@ -8,8 +8,20 @@ class TestJSONschemaValidator(unittest.TestCase):
     def setUp(self):
         self._data_dir = os.path.join(os.path.dirname(__file__), "data")
 
-    def test_convert_vendor(self):
+    def test_cedar_sample(self):
+        self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "sample_cedar_schema.json"))
+
+    def test_cedar_schema_required(self):
+        self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "sample_cedar_schema_required_name.json"))
+
+    def test_sample_schema(self):
+        self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "sample_schema.json"))
+
+    def test_sample_schema_required_name(self):
+        self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "sample_schema_required_name.json"))
+
+    def test_vendor_cedar_schema(self):
         self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "vendor_cedar_schema.json"))
 
-    def test_convert_sample(self):
-        self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "sample_cedar_schema.json"))
+    def test_vendor_schema(self):
+        self.assertTrue(util.jsonschema_validator.validate_schema(self._data_dir, "vendor_schema.json"))
