e2a9314c5bab69f0305729fbc85d145121ea584b
Dom <batistadominique@hotmail.com>
2018-05-22 11:34:22 +0100

    Created a separate config file with empty attribute for GitHub push; Created a method to test that all the attributes values are set in the config file

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,9 +8,14 @@ class CEDARClientTestCase(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(CEDARClientTestCase, self).__init__(*args, **kwargs)
-        configfile_path = os.path.join(os.path.dirname(__file__), "test_config.mine.json")
+
+        configfile_path = os.path.join(os.path.dirname(__file__), "test_config.json")
+        if not (os.path.exists(configfile_path)):
+            configfile_path = os.path.join(os.path.dirname(__file__), "test_config.json")
         with open(configfile_path) as config_data_file:
             config_json = json.load(config_data_file)
+            self.validate_config(config_json)
+
         self._data_dir = os.path.join(os.path.dirname(__file__), "data")
         self.production_api_key = config_json["production_key"]
         self.staging_api_key = config_json["staging_key"]
@@ -21,6 +26,28 @@ class CEDARClientTestCase(unittest.TestCase):
 
     def setUp(self):
         self.client = cedar.client.CEDARClient()
+
+    def validate_config(self, config_data):
+        try:
+            self.assertTrue(config_data['production_key'])
+        except AssertionError as inst:
+            print("Please provide an production API key before running tests")
+            raise
+        try:
+            self.assertTrue(config_data['staging_key'])
+        except AssertionError:
+            print("Please provide a staging API key before running tests")
+            raise
+        try:
+            self.assertTrue(config_data['folder_id'])
+        except AssertionError:
+            print("Please provide a valid folder ID before running tests")
+            raise
+        try:
+            self.assertTrue(config_data['template_id'])
+        except AssertionError:
+            print("Please provide a valid template ID before running tests")
+            raise
 
     def test_get_users(self):
         response = self.client.get_users("production", self.production_api_key)
