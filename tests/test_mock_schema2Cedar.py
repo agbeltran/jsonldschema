f7db7bc840a6288d2591fbc1a4759851ceaf5b88
Dom <batistadominique@hotmail.com>
2018-07-19 16:14:04 +0100

    Added mockup tests for testing Schema2CedarBase childrens

---
+++ tests/test_mock_schema2Cedar.py
@@ -0,0 +1,29 @@
+from mock import patch
+from nose.tools import assert_true, eq_
+from cedar.schema2Cedar import Schema2CedarTemplate, Schema2CedarTemplateElement
+import os
+import json
+from random import randint
+
+
+class TestSchema2Cedar(object):
+    @classmethod
+    def setup_class(cls):
+        cls.cedarTemplate = Schema2CedarTemplate()
+        cls.cedarTemplateElement = Schema2CedarTemplateElement()
+
+        cls.mock_request_patcher = patch('cedar.schema2Cedar.requests.request')
+        cls.mock_json_patcher = patch('cedar.schema2Cedar.json.loads')
+
+        cls.mock_request = cls.mock_request_patcher.start()
+        cls.mock_json = cls.mock_json_patcher.start()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.mock_request_patcher.stop()
+        cls.mock_json_patcher.stop()
+
+    def test_convert_template(self):
+        self.mock_request.return_value.status_code = 200
+        response = self.cedarTemplate.convert_template("tests/data/schema.json")
+        assert_true(response)
