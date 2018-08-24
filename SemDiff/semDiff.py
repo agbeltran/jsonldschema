79962e70e8ef8f8e778b5b91a0822508cf53dcdd
Dom <batistadominique@hotmail.com>
2018-08-24 11:08:46 +0100

    Refactored into a class

--- SemDiff/semDiff.py
+++ SemDiff/semDiff.py
@@ -197,89 +197,115 @@ schemasInput = {
 }
 
 
-def build_context_dict(schema_input):
+class SemanticComparator:
+    """
+     A class that compute the overlap between two JSON schemas semantic values taken from context files
+    """
 
-    sorted_values = {}
-    ignored_keys = ["@id", "@context", "@type"]
-    schema = copy.deepcopy(schema_input)
-    ignored_fields = []
+    def __init__(self, schema_a, context_a, schema_b, context_b):
+        self.input1 = {
+            "schema": schema_a,
+            "context": context_a
+        }
+        self.input2 = {
+            "schema": schema_b,
+            "context": context_b
+        }
 
-    # for each field in the schema
-    for field in schema['schema']['properties']:
+        self.comparator1 = self.build_context_dict(self.input1)
+        self.comparator2 = self.build_context_dict(self.input2)
+        self.overlaps = self.compute_context_coverage(self.comparator1[0], self.comparator2[0])
 
-        # Ignoring useless keys
-        if field not in ignored_keys:
+    def compute_coverage(self):
+        return {
+            "coverage": self.overlaps[0],
+            "overlapping fields": self.overlaps[1],
+            "ignored fields": self.comparator1[1]
+        }
 
-            # If the field can be found in the context, process it
-            if field in schema["context"]["@context"].keys():
+    def build_context_dict(self, schema_input):
+        sorted_values = {}
+        ignored_keys = ["@id", "@context", "@type"]
+        schema = copy.deepcopy(schema_input)
+        ignored_fields = []
 
-                # This is the raw semantic value of the field, it might need some processing
-                raw_semantic_value = schema["context"]["@context"][field]
+        # for each field in the schema
+        for field in schema['schema']['properties']:
 
-                # If the field raw semantic value is a string
-                if isinstance(raw_semantic_value, str):
-                    sorted_values = process_field(field,
-                                                  raw_semantic_value,
-                                                  schema["context"]["@context"],
-                                                  sorted_values)
+            # Ignoring useless keys
+            if field not in ignored_keys:
 
-                # if the field raw semantic value is not a string
+                # If the field can be found in the context, process it
+                if field in schema["context"]["@context"].keys():
+
+                    # This is the raw semantic value of the field, it might need some processing
+                    raw_semantic_value = schema["context"]["@context"][field]
+
+                    # If the field raw semantic value is a string
+                    if isinstance(raw_semantic_value, str):
+                        sorted_values = self.process_field(field,
+                                                           raw_semantic_value,
+                                                           schema["context"]["@context"],
+                                                           sorted_values)
+
+                    # if the field raw semantic value is not a string
+                    else:
+                        sorted_values = self.process_field(field,
+                                                           raw_semantic_value['@id'],
+                                                           schema["context"]["@context"],
+                                                           sorted_values)
+
+                # if the field is absent from the context file, ignore it as it has no semantic definition
                 else:
-                    sorted_values = process_field(field,
-                                                  raw_semantic_value['@id'],
-                                                  schema["context"]["@context"],
-                                                  sorted_values)
+                    ignored_fields.append(field)
 
-            # if the field is absent from the context file, ignore it as it has no semantic definition
+        return sorted_values, ignored_fields
+
+    @staticmethod
+    def process_field(field_name, field_value, context, comparator):
+
+        base_url = urlparse(field_value).scheme
+
+        # if the raw value is already an URL, it does not need processing
+        if base_url in ('http', 'https'):
+            if field_value not in comparator:
+                comparator[field_value] = [field_name]
             else:
-                ignored_fields.append(field)
+                comparator[field_value].append(field_name)
 
-    return sorted_values, ignored_fields
-
-
-def process_field(field_name, field_value, context, comparator):
-
-    base_url = urlparse(field_value).scheme
-
-    # if the raw value is already an URL, it does not need processing
-    if base_url in ('http', 'https'):
-        if field_value not in comparator:
-            comparator[field_value] = [field_name]
+        # replacing semantic base to form an absolute IRI
         else:
-            comparator[field_value].append(field_name)
+            processed_semantic_value = field_value.replace(base_url + ":", context[base_url])
+            if processed_semantic_value not in comparator:
+                comparator[processed_semantic_value] = [field_name]
+            else:
+                comparator[processed_semantic_value].append(field_name)
 
-    # replacing semantic base to form an absolute IRI
-    else:
-        processed_semantic_value = field_value.replace(base_url + ":", context[base_url])
-        if processed_semantic_value not in comparator:
-            comparator[processed_semantic_value] = [field_name]
-        else:
-            comparator[processed_semantic_value].append(field_name)
+        return comparator
 
-    return comparator
+    @staticmethod
+    def compute_context_coverage(context1, context2):
 
+        Overlap = namedtuple('Overlap', ['first_field', 'second_field'])
+        OverlapValue = namedtuple('OverlapValue', ['relative_coverage', 'absolute_coverage'])
 
-def compute_context_coverage(context1, context2):
+        overlap_number = 0
+        overlap_output = []
+        for field in context1:
+            if field in context2:
+                overlap_number += len(context1[field])
 
-    Overlap = namedtuple('Overlap', ['first_field', 'second_field'])
-    OverlapValue = namedtuple('OverlapValue', ['relative_coverage', 'absolute_coverage'])
+                for first_field_val in context1[field]:
+                    for second_field_val in context2[field]:
+                        local_overlap = Overlap(first_field_val, second_field_val)
+                        overlap_output.append(local_overlap)
 
-    overlap_number = 0
-    overlap_output = []
-    for field in context1:
-        if field in context2:
-            overlap_number += len(context1[field])
+        local_overlap_value = OverlapValue(str(round((overlap_number * 100) / len(context1), 2)), str(overlap_number))
 
-            for first_field_val in context1[field]:
-                for second_field_val in context2[field]:
-                    local_overlap = Overlap(first_field_val, second_field_val)
-                    overlap_output.append(local_overlap)
-
-    local_overlap_value = OverlapValue(str(round((overlap_number * 100) / len(context1), 2)), overlap_number)
-
-    return local_overlap_value, overlap_output
+        return local_overlap_value, overlap_output
 
 
+"""
 def compute_coverage(schema1, context1, schema2, context2):
 
     input1 = {
@@ -296,11 +322,6 @@ def compute_coverage(schema1, context1, schema2, context2):
     comparator2 = build_context_dict(input2)
 
     coverage = compute_context_coverage(comparator1[0], comparator2[0])
-    print("----- \nCoverage: " + coverage[0].relative_coverage + "%")
-    print("----- \nOverlapping fields:")
-    print(coverage[1])
-    print("----- \nIgnored fields:")
-    print(comparator1[1])
 
     return {
         "coverage": coverage[0],
@@ -308,8 +329,8 @@ def compute_coverage(schema1, context1, schema2, context2):
         "ignored fields": comparator1[1]
     }
 
+"""
 
 if __name__ == "__main__":
-    output = compute_coverage(personC, personA_context,
-                              personB, personB_context)
-    print(output)
+    output = SemanticComparator(personC, personA_context, personB, personB_context)
+    print(output.compute_coverage())
