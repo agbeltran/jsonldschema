c43e2ee19d37c16aabce283d25c46a4bb80f9afc
Dom <batistadominique@hotmail.com>
2018-09-05 14:51:06 +0100

    Fixed names and added base

--- validate/schema2context.py
+++ validate/schema2context.py
@@ -6,12 +6,14 @@ def create_context(schema, semantic_types, name):
         for field in schema:
             contexts[semantic_type][field] = semantic_type+':'
         contexts[semantic_type][name] = semantic_type+':'
+        contexts[semantic_type][semantic_type] = semantic_types[semantic_type]
+        contexts[semantic_type]["@language"] = "en"
 
     return contexts
 
 
 def process_schema_name(name):
-    new_raw_name = name.replace("_schema.json", '')
+    new_raw_name = name.replace("_schema.json", '').replace('.json', '')
     name_array = new_raw_name.split('_')
     output_name = ""
 
@@ -53,7 +55,17 @@ if __name__ == '__main__':
         },
         "additionalProperties": False
     }
-    schema_name = process_schema_name('person_schema.json')
-    base = ['sdo', 'obo']
+    schema_name = process_schema_name('identifier_info_schema.json')
+    base = {
+        "sdo": "https://schema.org",
+        "obo": "http://purl.obolibrary.org/obo/"
+    }
+
     new_context = create_context(person_schema, base, schema_name)
-    print(new_context)
+    sdo_context_name = 'identifier_info_schema.json'.replace('_schema', '_sdo_context')
+    obo_context_name = 'identifier_info_schema.json'.replace('_schema', '_obo_context')
+
+    print(sdo_context_name)
+    print(new_context['sdo'])
+    print(obo_context_name)
+    print(new_context['obo'])
