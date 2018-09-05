8b85b112fa60cf3918d0068e2fdc2e2532616758
Dom <batistadominique@hotmail.com>
2018-09-05 11:54:41 +0100

    Fixed semDiff to comply with PEP8

--- SemDiff/semDiff.py
+++ SemDiff/semDiff.py
@@ -5,7 +5,8 @@ from collections import namedtuple
 
 class SemanticComparator:
     """
-     A class that compute the overlap between two JSON schemas semantic values taken from context files
+     A class that compute the overlap between two JSON schemas semantic values taken from context
+     files
      :param schema_a: the content of the first schema
      :param context_a: the context content bound to the first schema
      :param schema_b: the content of the second schema
@@ -34,11 +35,12 @@ class SemanticComparator:
 
     def __build_context_dict(self, schema_input):
         """
-        A private method that associate each field in a schema to it's semantic value in the context and reverse the
-        result
+        A private method that associate each field in a schema to it's semantic value in the
+        context and reverse the result
         :param schema_input:
         :return sorted_values: a dictionary of semantic values and their corresponding field
-        :return ignored_fields: a list of fields that were ignored due to having no semantic value in the context file
+        :return ignored_fields: a list of fields that were ignored due to having no semantic value
+        in the context file
         """
         sorted_values = {}
         ignored_keys = ["@id", "@context", "@type"]
@@ -71,7 +73,8 @@ class SemanticComparator:
                                                              schema["context"]["@context"],
                                                              sorted_values)
 
-                # if the field is absent from the context file, ignore it as it has no semantic definition
+                # if the field is absent from the context file, ignore it as it has no semantic
+                # definition
                 else:
                     ignored_fields.append(field)
 
@@ -80,12 +83,14 @@ class SemanticComparator:
     @staticmethod
     def __process_field(field_name, field_value, context, comparator):
         """
-        Private method that catches a given field semantic value from the given context and adds it to the output
+        Private method that catches a given field semantic value from the given context and adds it
+        to the output
         :param field_name: the name of the given field
         :param field_value: the value of the given field
         :param context: the context from which to retrieve the semantic value
         :param comparator: the output of __build_context_dict()
-        :return comparator: a dictionary of semantic values and corresponding fields from the given schema and context
+        :return comparator: a dictionary of semantic values and corresponding fields from the given
+        schema and context
         """
 
         base_url = urlparse(field_value).scheme
@@ -114,7 +119,8 @@ class SemanticComparator:
         :param context1: the final output of __build_context_dict() for the first schema
         :param context2: the final output of __build_context_dict() for the second schema
         :return local_overlap_value: a namedtuple containing relative and absolute coverage
-        :return overlap_output: a dictionary that associate fields in schema 1 with their semantic twins in schema 2
+        :return overlap_output: a dictionary that associate fields in schema 1 with their semantic
+        twins in schema 2
         """
 
         Overlap = namedtuple('Overlap', ['first_field', 'second_field'])
@@ -125,7 +131,7 @@ class SemanticComparator:
         processed_field = 0
 
         for field in context1:
-            processed_field+=1
+            processed_field += 1
             if field in context2:
                 overlap_number += len(context1[field])
 
@@ -136,6 +142,7 @@ class SemanticComparator:
 
         absolute_coverage = namedtuple('AbsoluteCoverage', ['overlap_number', 'total_fields'])
         AbsoluteCoverage = absolute_coverage(str(overlap_number), str(processed_field))
-        local_overlap_value = OverlapValue(str(round((overlap_number * 100) / len(context1), 2)), AbsoluteCoverage)
+        local_overlap_value = OverlapValue(str(round((overlap_number * 100) / len(context1), 2)),
+                                           AbsoluteCoverage)
 
         return local_overlap_value, overlap_output
