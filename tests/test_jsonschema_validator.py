8e66f3a79afd442a57b959345acdfaaeb517b4bf
Dom <batistadominique@hotmail.com>
2018-09-05 13:05:29 +0100

    PEP8 compliance for jsonschema_validator unittests

--- tests/test_jsonschema_validator.py
+++ tests/test_jsonschema_validator.py
@@ -10,28 +10,41 @@ class TestJSONschemaValidator(unittest.TestCase):
 
     def test_annotated_sample_cedar(self):
         self.assertTrue(
-            validate.jsonschema_validator.validate_schema(self._data_dir, "annotated_sample_cedar_schema.json"))
+            validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                          "annotated_sample_cedar_schema.json"))
 
     def test_sample_cedar(self):
-        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir, "sample_cedar_schema.json"))
+        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                                      "sample_cedar_schema.json"))
 
     def test_sample_cedar_required_name(self):
         self.assertTrue(
-            validate.jsonschema_validator.validate_schema(self._data_dir, "sample_required_name_cedar_schema.json"))
+            validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                          "sample_required_name_cedar_schema.json")
+        )
 
     def test_sample(self):
-        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir, "sample_schema.json"))
+        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                                      "sample_schema.json"))
 
     def test_sample_required_name(self):
         self.assertTrue(
-            validate.jsonschema_validator.validate_schema(self._data_dir, "sample_required_name_schema.json"))
+            validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                          "sample_required_name_schema.json"))
 
     def test_vendor_cedar(self):
-        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir, "vendor_cedar_schema.json"))
+        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                                      "vendor_cedar_schema.json"))
 
     def test_vendor(self):
-        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir, "vendor_schema.json"))
+        self.assertTrue(validate.jsonschema_validator.validate_schema(self._data_dir,
+                                                                      "vendor_schema.json"))
 
     def test_sample_instance(self):
-        errors = validate.jsonschema_validator.validate_instance(self._data_dir, "sample_schema.json", self._data_dir, "sample_data.json", 1, {})
-        self.assertTrue(errors.__len__()==0)
+        errors = validate.jsonschema_validator.validate_instance(self._data_dir,
+                                                                 "sample_schema.json",
+                                                                 self._data_dir,
+                                                                 "sample_data.json",
+                                                                 1,
+                                                                 {})
+        self.assertTrue(errors.__len__() == 0)
