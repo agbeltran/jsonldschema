ec367d803ab1aec6c9a0599081edb8c01f908d69
Terazus <batistadominique@hotmail.com>
2018-09-20 12:18:05 +0100

    Starting computing validation with mapping dict

--- validate/miflowcyt_validate.py
+++ validate/miflowcyt_validate.py
@@ -1,13 +1,9 @@
 from requests import request
-from xmljson import parker, Parker
+from xmljson import parker
 from xml.etree.ElementTree import fromstring
 from json import dumps, load
 import os
-
-
-clientID = "DBasdfas89798asoj892KOS"
-itemID = "FR-FCM-ZY68"
-base_schema = "experiment_schema.json"
+import pprint
 
 
 def grab_user_content(client_identifier):
@@ -17,48 +13,80 @@ def grab_user_content(client_identifier):
 
 
 def grab_experiment_from_api(client_identifier, item_identifier):
+    """
+    Retrieve the experiment metadata and return it as a python object
+    :param client_identifier: the client identifier (apiKey)
+    :param item_identifier: the item identifier that should be retrieved
+    :return: the python object obtained from the XML
+    """
 
-    """test = os.path.join(os.path.dirname(__file__), "../"+base_schema)
-
-    with open(test) as schema_input:
-        schema_data = load(schema_input)
-    schema_input.close()"""
-
-    full_url = "http://flowrepository.org/list/" + item_identifier \
-                 + "?client=" + client_identifier
+    full_url = "http://flowrepository.org/list/" + item_identifier + "?client=" + client_identifier
     response = request("GET", full_url)
     return parker.data((fromstring(response.text)))["public-experiments"]["experiment"]
 
-    """total_field_number = 0
-    valid_field_number = 0
 
-    for fieldName in schema_data["properties"]:
-        print(fieldName)
-        total_field_number += 1
+def load_schema(base_schema_name, network):
+    """
+    Load the set of schemas dependencies from the base given schema
+    :param base_schema_name: the name of the schema to load
+    :param network: the dictionary to add the loaded schemas
+    :return network: the dictionary that contains the loaded schemas
+    """
 
-        if fieldName in input_data:
-            valid_field_number += 1
-
-    print(total_field_number)
-    print(valid_field_number)
-
-    print(input_data)"""
-
-
-def load_schemas(base_schema_name):
-
-    network = {}
-
-    base_schema_path = os.path.join(os.path.dirname(__file__), ".././tests/data/MiFlowCyt/"+base_schema_name)
+    base_path = ".././tests/data/MiFlowCyt/"
+    base_schema_path = os.path.join(os.path.dirname(__file__), base_path + base_schema_name)
     with open(base_schema_path) as schema_input:
         schema_data = load(schema_input)
     schema_input.close()
 
-    print(schema_data)
+    network[base_schema_name] = schema_data
 
-    return 0
+    for field in schema_data["properties"]:
+
+        # TODO: add use case for anyOf, oneOf and items
+        if '$ref' in schema_data["properties"][field]:
+            test = schema_data["properties"][field]["$ref"].rsplit('/', 1)[-1]
+            network = load_schema(test, network)
+
+    return network
+
+
+def validate_instance(instance, schema, mapping):
+    ignored_keys = ["@id", "@type", "@context"]
+    #pp = pprint.PrettyPrinter(indent=2)
+    #pp.pprint(instance)
+
+    total_fields = 0
+    matched_field = 0
+
+    for field in schema['properties']:
+        if field not in ignored_keys:
+
+            total_fields += 1
+            if field in mapping:
+                processing_field = mapping[field]
+            else:
+                processing_field = field
+
+            if processing_field in instance:
+                matched_field +=1
+
+    print("Matched: " + str(matched_field) + " out of " + str(total_fields))
 
 
 if __name__ == '__main__':
+    clientID = "DBasdfas89798asoj892KOS"
+    itemID = "FR-FCM-ZY68"
+    base_schema = "experiment_schema.json"
+
+    mapping_dict = {
+        "qualityControlMeasures": "quality-control-measures",
+        "conclusions": "conclusion",
+        "organization": "organizations",
+        "date": "experiment-dates",
+        "primaryContact": "primary-researcher"
+    }
+
     data = grab_experiment_from_api(clientID, itemID)
-    schemas = load_schemas(base_schema)
+    schemas = load_schema(base_schema, {})
+    validate_instance(data, schemas['experiment_schema.json'], mapping_dict)
