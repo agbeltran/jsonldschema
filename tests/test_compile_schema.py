60b5b8929ad32e5838c7f37fa8cfd8bc6ae45c3b
Dom <batistadominique@hotmail.com>
2018-11-03 14:30:25 +0000

    fixed test_cimpile_schema for travis build

--- tests/test_compile_schema.py
+++ tests/test_compile_schema.py
@@ -1,8 +1,9 @@
 import unittest
-from utils import compile_schema
+import os
 import json
 from deepdiff import DeepDiff
 from jsonschema.validators import RefResolver
+from utils import compile_schema
 
 
 class CompileSchemaTestCase(unittest.TestCase):
@@ -12,13 +13,19 @@ class CompileSchemaTestCase(unittest.TestCase):
 
     def setUp(self):
         self.schema_URL = "https://w3id.org/dats/schema/study_schema.json#"
+        mapping_dir = os.path.join(os.path.dirname(__file__), "data")
+
+        with open(os.path.join(mapping_dir, "study_schema.json")) as file_opened:
+            self.study_schema = json.load(file_opened)
+            file_opened.close()
+        with open(os.path.join(mapping_dir, "compile_test.json")) as file_opened:
+            self.expected_output = json.load(file_opened)
+            file_opened.close()
 
     def test_resolve_reference(self):
-        expected_output = json.load(open('data/study_schema.json'))
-
         ref_resolved = compile_schema.resolve_reference(self.schema_URL)
         output_value = json.loads(json.dumps(ref_resolved))
-        output_expected = json.loads(json.dumps(expected_output))
+        output_expected = json.loads(json.dumps(self.study_schema))
         self.assertTrue(DeepDiff(output_value, output_expected) == {})
 
         ref_2_resolved = compile_schema.resolve_reference("fakeURL")
@@ -32,8 +39,7 @@ class CompileSchemaTestCase(unittest.TestCase):
 
     def test_resolve_schema_references(self):
 
-        expected_output = json.load(open('data/compile_test.json'))
-
+        expected_output = self.expected_output
         processed_schemas = {}
         schema_url = 'https://w3id.org/dats/schema/person_schema.json#'
         processed_schemas[compile_schema.get_name(schema_url)] = '#'
@@ -58,6 +64,6 @@ class CompileSchemaTestCase(unittest.TestCase):
                                                          '#')
 
         output_value = json.loads(json.dumps(data))
-        expected_output = json.loads(json.dumps(json.load(open('data/compile_test.json'))))
+        expected_output = json.loads(json.dumps(self.expected_output))
 
         self.assertTrue(DeepDiff(output_value, expected_output) == {})
