9d8af5e9b1e0e1e039626c65259d6f952a0de34e
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2018-11-07 15:38:27 +0000

    Changes to test aiming at fixing assertion

--- tests/test_compile_schema.py
+++ tests/test_compile_schema.py
@@ -2,6 +2,9 @@ from collections import OrderedDict
 from nose.tools import eq_
 from mock import patch
 from utils import compile_schema
+import os
+import json
+from deepdiff import diff
 
 
 class TestCaseSchemaCompiler(object):
@@ -13,20 +16,20 @@ class TestCaseSchemaCompiler(object):
     def setup_class(cls):
         cls.mock_resolver_patcher = patch('utils.compile_schema.RefResolver.resolve')
         cls.mock_resolver = cls.mock_resolver_patcher.start()
-        cls.mock_json_patcher = patch('utils.compile_schema.json.loads')
-        cls.mock_json = cls.mock_json_patcher.start()
         cls.compiler = compile_schema
 
     @classmethod
     def teardown_class(cls):
         cls.mock_resolver_patcher.stop()
-        cls.mock_json_patcher.stop()
+        #cls.mock_json_patcher.stop()
 
     def test_get_name(self):
         expected_name = self.compiler.get_name("https://w3id.org/dats/schema/study_schema.json#")
         eq_(expected_name, 'study_schema.json')
 
     def test_resolve_reference(self):
+        self.mock_json_patcher = patch('utils.compile_schema.json.loads')
+        self.mock_json = self.mock_json_patcher.start()
         self.mock_json.return_value = "{'test':'test'}"
         tested_output = self.compiler.resolve_reference(
             "https://w3id.org/dats/schema/person_schema.json#")
@@ -48,31 +51,13 @@ class TestCaseSchemaCompiler(object):
 
     def test_resolve_schema_references(self):
 
-        schema = OrderedDict({
-            "id": "schemas/test.json#",
-            "definitions": {
-                "field_1": {"$ref": "second_test.json#"}
-            },
-            "properties": OrderedDict({
-                "field_1": {
-                    "$ref": "second_test.json#"
-                },
-                "field_2": {
-                    "type": "array",
-                    "items": {
-                        "anyOf": [
-                            {
-                                "$ref": "second_test.json"
-                            }
-                        ]
-                    }
-                }
-            })
-        })
+        data_path = os.path.join(os.path.dirname(__file__), "./data")
+        schema = json.loads(open(os.path.join(data_path, "schema_field1_field2.json")).read(), object_pairs_hook=OrderedDict)
+
         loaded_schemas = {
             "test.json": schema,
         }
-        self.mock_json_patcher.start()
+        #self.mock_json_patcher.start()
         self.mock_resolver.return_value = ["", {
             "id": "schemas/second_test.json",
             "properties": {
@@ -83,33 +68,11 @@ class TestCaseSchemaCompiler(object):
             }
         }]
 
-        expected_output = OrderedDict({
-            "id": "schemas/test.json#",
-            "definitions": {
-                "field_1": OrderedDict({"$ref": "#/properties/field_1"})
-            },
-            "properties": {
-                "field_1": OrderedDict({
-                    "id": "schemas/second_test.json",
-                    "properties": {
-                        "name": OrderedDict({
-                            "description": "test description",
-                            "type": "string"
-                        })
-                    }
-                }),
-                "field_2": OrderedDict({
-                    "type": "array",
-                    "items": OrderedDict({
-                        "anyOf": [{
-                            "$ref": "#/properties/field_1"
-                        }]
-                    })
-                })
-            }
-        })
+        expected_output = json.loads(open(os.path.join(data_path, "expected_output_field1_field2.json")).read()) #, object_pairs_hook=OrderedDict)
+        print("expected output --->", expected_output)
 
         output_json = self.compiler.resolve_schema_references(schema, loaded_schemas)
+        print("------>", diff.DeepDiff(output_json, expected_output))
         eq_(output_json, expected_output)
 
         output_json = self.compiler.resolve_schema_references(schema, loaded_schemas, "123")
