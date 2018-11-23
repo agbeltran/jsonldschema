532b15fef0cbf91f4203457061904746e04ba4ab
Dom <batistadominique@hotmail.com>
2018-11-23 11:15:14 +0000

    adding unittests for to_boolean function

---
+++ tests/test_to_bolean.py
@@ -0,0 +1,12 @@
+import unittest
+import utils
+
+
+class ToBooleanTestCase(unittest.TestCase):
+
+    def test_to_bolean(self):
+        val = utils.to_boolean("yes")
+        self.assertTrue(val is True)
+
+        val2 = utils.to_boolean("no")
+        self.assertTrue(val2 is False)
