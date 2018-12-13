d1f8a636f214362b9b6954bd977202b8247998ab
Dom <batistadominique@hotmail.com>
2018-12-13 10:53:21 +0000

    Fixed unittests for PR #71

--- tests/test_prepare_fulldiff.py
+++ tests/test_prepare_fulldiff.py
@@ -9,19 +9,30 @@ class TestCasePrepareFullDiffInput(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestCasePrepareFullDiffInput, self).__init__(*args, **kwargs)
-        self.mock_resolver_patcher = patch('utils.prepare_fulldiff_input.RefResolver.resolve')
-        self.mock_resolver = self.mock_resolver_patcher.start()
-
-        self.mock_request_patcher = patch('utils.prepare_fulldiff_input.requests.get')
-        self.mock_request = self.mock_request_patcher.start()
-
         self.pre_process = prepare_fulldiff_input
+
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.mock_resolver_patcher = patch('utils.prepare_fulldiff_input.RefResolver.resolve')
+        cls.mock_resolver = cls.mock_resolver_patcher.start()
+
+        cls.mock_request_patcher = patch('utils.prepare_fulldiff_input.requests.get')
+        cls.mock_request = cls.mock_request_patcher.start()
+
+        cls.pre_process = prepare_fulldiff_input
+    """
 
     @classmethod
     def teardown_class(cls):
-        cls.mock_resolver_patcher.stop()
+        print("BYE")
+        # cls.mock_resolver_patcher.stop()
 
     def test_load_context(self):
+
+        mock_request_patcher = patch('utils.prepare_fulldiff_input.requests.get')
+        mock_request = mock_request_patcher.start()
+
         contexts_mapping = {
             "networkName": "test1",
             "contexts": {
@@ -31,27 +42,31 @@ class TestCasePrepareFullDiffInput(unittest.TestCase):
 
         context = {
             "@context":  {
-                    'test': "sdo:test"
-                }
+                'test': "sdo:test"
+            }
         }
 
         class RequestMockResponse:
             text = json.dumps(context)
 
         expected_output = {'test_schema.json': {'test': 'sdo:test'}}
-        self.mock_request.return_value = RequestMockResponse
+        mock_request.return_value = RequestMockResponse
         tested_output = self.pre_process.load_context(contexts_mapping)
         eq_(tested_output, expected_output)
+        mock_request_patcher.stop()
 
-        self.mock_request_patcher.stop()
         with self.assertRaises(Exception) as context:
             self.pre_process.load_context(contexts_mapping)
             self.assertTrue('There is a problem with your url or schema'
                             in context.exception)
 
     def test_resolve_network(self):
-        schema_url = "justatest.com"
+        mock_resolver_patcher = patch('utils.prepare_fulldiff_input.RefResolver.resolve')
+        mock_resolver = mock_resolver_patcher.start()
+        mock_request_patcher = patch('utils.prepare_fulldiff_input.requests.get')
+        mock_request = mock_request_patcher.start()
 
+        schema_url = "justatest.com"
         schema = {
             "id": "schemas/test.json#",
             "definitions": {
@@ -72,7 +87,7 @@ class TestCasePrepareFullDiffInput(unittest.TestCase):
             }
         }
 
-        self.mock_resolver.return_value = ["", {
+        mock_resolver.return_value = ["", {
             "id": "schemas/second_test.json",
             "properties": {
                 "name": {
@@ -84,18 +99,19 @@ class TestCasePrepareFullDiffInput(unittest.TestCase):
 
         expected_output = {
             "test.json": schema,
-            "second_test.json": self.mock_resolver.return_value[1]
+            "second_test.json": mock_resolver.return_value[1]
         }
 
         class RequestMockResponse:
             text = json.dumps(schema)
 
-        self.mock_request.return_value = RequestMockResponse
+        mock_request.return_value = RequestMockResponse
         tested_output = self.pre_process.resolve_network(schema_url)
         eq_(tested_output, expected_output)
-        self.mock_request_patcher.stop()
+        mock_request_patcher.stop()
 
         with self.assertRaises(Exception) as context:
             self.pre_process.resolve_network(schema_url)
             self.assertTrue('There is a problem with your url or schema'
                             in context.exception)
+        mock_resolver_patcher.stop()
